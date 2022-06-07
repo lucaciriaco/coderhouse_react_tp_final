ddc41ea4fc8a8dc714fc88c3a723303d84030019
lucaciriaco <33738825+lucaciriaco@users.noreply.github.com>
2022-06-06 22:13:21 -0300

    Trabajo practico numero 2

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import './Navbar.css'
+export default function Navbar (){
+    return(
+        <nav>
+            <ul>
+                <li><a href="#">Tu tienda</a></li>
+                <li><a href="#">Libreria</a></li>
+                <li><a href="#">Novedades</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="#">Noticias</a></li>
+            </ul>
+        </nav>
+    );
+}
